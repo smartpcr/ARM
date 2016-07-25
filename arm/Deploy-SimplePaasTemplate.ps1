642a17500c4a84a39736e39906b2c1044f6507de
xiaodoli <xiaodoli@microsoft.com>
2016-07-24 21:18:08 -0700

    paas template

---
+++ arm/Deploy-SimplePaasTemplate.ps1
@@ -0,0 +1,26 @@
+﻿### Define variables
+{
+$location = 'West US 2'
+$resourceGroupName = 'pluralsight-arm-simple-paas-template'
+$resourceDeploymentName = 'pluralsight-arm-paas-template-deployment'
+$templatePath = $PWD
+$templateFile = 'simplePaas.json'
+$template = Join-Path $templatePath $templateFile -Resolve
+}
+
+### Create Resource Group
+{
+New-AzureRmResourceGroup `
+    -Name $resourceGroupName `
+    -Location $Location `
+    -Verbose -Force
+}
+
+### Deploy Resources
+{
+New-AzureRmResourceGroupDeployment `
+    -Name $resourceDeploymentName `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $template `
+    -Verbose -Force
+}
